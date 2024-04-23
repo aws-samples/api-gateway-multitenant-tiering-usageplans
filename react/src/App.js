1bb37ce41c9b161b631d693c5ced80a6063937fb
Aymen Naghmouchi <aymenadvance@gmail.com>
2024-04-23 16:48:38 +0200

    src: fix import of amplify

--- react/src/App.js
+++ react/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import Amplify from "aws-amplify";
+import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
