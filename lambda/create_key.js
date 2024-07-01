0f43a4203497ca9fa6bee3ec033773a077456464
zaguezzar <zaguezzar@gmail.com>
2024-07-01 21:28:34 +0100

    fix: create_key & get_keys lambdas

--- lambda/create_key.js
+++ lambda/create_key.js
@@ -1,188 +1,194 @@
-const AWS = require("aws-sdk");
-const {findPoolForPlanId} = require("./api_key_pools");
-
 const {
-  parseJwt,
-  goodResponse,
-  internalServerErrorResponse,
-  makeid,
-} = require("./utils");
+	DynamoDBClient,
+	GetItemCommand,
+	PutItemCommand,
+} = require('@aws-sdk/client-dynamodb');
+const {
+	APIGatewayClient,
+	CreateApiKeyCommand,
+	CreateUsagePlanKeyCommand,
+} = require('@aws-sdk/client-api-gateway');
+const { unmarshall } = require('@aws-sdk/util-dynamodb');
+const { findPoolForPlanId } = require('./api_key_pools');
+const {
+	parseJwt,
+	goodResponse,
+	internalServerErrorResponse,
+	makeid,
+} = require('./utils');
 
-const dynamo = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
-var apigateway = new AWS.APIGateway({ apiVersion: "2015-07-09" });
+const dynamo = new DynamoDBClient({ region: 'eu-north-1' }); // replace REGION with your AWS region
+var apigateway = new APIGatewayClient({ region: 'eu-north-1' }); // replace REGION with your AWS region
 
-exports.handler = function (event, context, callback) {
-  console.log("Received event:", JSON.stringify(event, null, 2));
-  console.log("Received context: ", JSON.stringify(context, null, 2));
+exports.handler = async function (event, context) {
+	console.log('Received event:', JSON.stringify(event, null, 2));
+	console.log('Received context: ', JSON.stringify(context, null, 2));
 
-  const plansTable = process.env.PLANS_TABLE_NAME;
-  const keysTable = process.env.KEYS_TABLE_NAME;
+	const plansTable = process.env.PLANS_TABLE_NAME;
+	const keysTable = process.env.KEYS_TABLE_NAME;
 
-  const httpMethod = event.httpMethod; // e.g. "GET"
-  const path = event.path; // e.g. "/admin/plans/123456
-  const resource = event.resource; // e.g. "/admin/plans/{id}
-  const token = event.headers.Authorization.replace(/^[Bb]earer\s+/, "").trim();
+	const httpMethod = event.httpMethod; // e.g. "GET"
+	const path = event.path; // e.g. "/admin/plans/123456
+	const resource = event.resource; // e.g. "/admin/plans/{id}
+	//const token = event.headers.Authorization.replace(/^[Bb]earer\s+/, '').trim();
+	if (!event.headers || !event.headers.Authorization) {
+        console.error('HTTP 500: Authorization header missing');
+        return internalServerErrorResponse();
+    }
 
-  if (
-    !plansTable ||
-    !keysTable ||
-    !path ||
-    !resource ||
-    !httpMethod ||
-    !token
-  ) {
-    console.error(
-      `HTTP 500: Precondition Fail: '${httpMethod}' '${path}' '${resource}' '${plansTable}' '${keysTable}' token.len=${token.len} `
-    );
-    callback(null, internalServerErrorResponse());
-    return;
-  }
-  const jwt = parseJwt(token);
-  console.log("JWT payload: ", JSON.stringify(jwt, null, 2));
+    const token = event.headers.Authorization.split(' ')[1];
 
-  const key = JSON.parse(event.body);
-  const rand = event.requestContext.requestId;
-  createKey(keysTable, key, plansTable, jwt, rand, callback);
+	
+	if (
+		!plansTable ||
+		!keysTable ||
+		!path ||
+		!resource ||
+		!httpMethod ||
+		!token
+	) {
+		console.error(
+			`HTTP 500: Precondition Fail: '${httpMethod}' '${path}' '${resource}' '${plansTable}' '${keysTable}' token.len=${token.len} `
+		);
+		return internalServerErrorResponse();
+	}
+	const jwt = parseJwt(token);
+	console.log('JWT payload: ', JSON.stringify(jwt, null, 2));
+
+	const key = JSON.parse(event.body);
+	const rand = event.requestContext.requestId;
+	await createKey(keysTable, key, plansTable, jwt, rand);
 };
 
 /**
  * POST /admin/keys
  */
-function createKey(tableName, key, plansTable, jwt, rand, callback) {
-  const pool = findPoolForPlanId(key.planId);
-  //  const pool = pools.apiKeyPools.find((item) => item.planId === key.planId);
+async function createKey(tableName, key, plansTable, jwt, rand) {
+	const pool = findPoolForPlanId(key.planId);
 
-  if (!pool) {
-    createSiloedKey(tableName, key, plansTable, jwt, rand, callback);
-  } else {
-    createPooledKey(pool, tableName, key, jwt, callback);
-  }
+	if (!pool) {
+		await createSiloedKey(tableName, key, plansTable, jwt, rand);
+	} else {
+		await createPooledKey(pool, tableName, key, jwt);
+	}
 }
 
 /**
  * POST /admin/keys
  */
-function createSiloedKey(tableName, key, plansTable, jwt, rand, callback) {
-  console.log("createSiloedKey");
+async function createSiloedKey(tableName, key, plansTable, jwt, rand) {
+	console.log('createSiloedKey');
 
-  var apiKeyId = undefined;
+	var apiKeyId = undefined;
 
-  // first make sure we have a valid plan ID
-  dynamo
-    .getItem({
-      TableName: plansTable,
-      Key: {
-        id: {
-          S: key.planId,
-        },
-      },
-    })
-    .promise()
-    .then((data) => {
-      console.log("Found matching plan: ", data);
-      // create an API key
-      return apigateway
-        .createApiKey({
-          name: key.name,
-          description: key.description,
-          enabled: key.enabled,
-          tags: { ownerId: jwt.sub },
-          value: rand,
-        })
-        .promise();
-    })
-    .then((apiKey) => {
-      console.log("APIGateway created APIKey: ", apiKey);
-      apiKeyId = apiKey.id;
-      return apigateway
-        .createUsagePlanKey({
-          keyId: apiKey.id,
-          keyType: "API_KEY",
-          usagePlanId: key.planId,
-        })
-        .promise();
-    })
-    .then((data) => {
-      console.log("APIGateway regsitered key with usage plan ", data);
-      // now save do our database.
-      return dynamo
-        .putItem({
-          TableName: tableName,
-          Item: {
-            id: { S: apiKeyId },
-            planId: { S: key.planId },
-            name: { S: key.name },
-            description: { S: key.description },
-            enabled: { BOOL: key.enabled },
-            owner: { S: jwt.sub },
-            value: { S: rand },
-          },
-        })
-        .promise();
-    })
-    .then((data) => {
-      console.log("Dynamo data: ", JSON.stringify(data.Item, null, 2));
-      const response = goodResponse(
-        JSON.stringify(AWS.DynamoDB.Converter.unmarshall(data.Item))
-      );
-      callback(null, response);
-    })
-    .catch((reason) => {
-      console.error(reason);
-      callback(null, internalServerErrorResponse());
-    });
+	// first make sure we have a valid plan ID
+	const data = await dynamo.send(
+		new GetItemCommand({
+			TableName: plansTable,
+			Key: {
+				id: {
+					S: key.planId,
+				},
+			},
+		})
+	);
+
+	console.log('Found matching plan: ', data);
+
+	// create an API key
+	const apiKey = await apigateway.send(
+		new CreateApiKeyCommand({
+			name: key.name,
+			description: key.description,
+			enabled: key.enabled,
+			tags: { ownerId: jwt.sub },
+			value: rand,
+		})
+	);
+
+	console.log('APIGateway created APIKey: ', apiKey);
+	apiKeyId = apiKey.id;
+
+	await apigateway.send(
+		new CreateUsagePlanKeyCommand({
+			keyId: apiKey.id,
+			keyType: 'API_KEY',
+			usagePlanId: key.planId,
+		})
+	);
+
+	console.log('APIGateway registered key with usage plan ');
+
+	// now save do our database.
+	await dynamo.send(
+		new PutItemCommand({
+			TableName: tableName,
+			Item: {
+				id: { S: apiKeyId },
+				planId: { S: key.planId },
+				name: { S: key.name },
+				description: { S: key.description },
+				enabled: { BOOL: key.enabled },
+				owner: { S: jwt.sub },
+				value: { S: rand },
+			},
+		})
+	);
+
+	console.log('Dynamo data: ', JSON.stringify(data.Item, null, 2));
+  console.log( goodResponse(
+		JSON.stringify(unmarshall(data.Item))
+	))
+
+	return goodResponse(
+		JSON.stringify(unmarshall(data.Item))
+	);
 }
 
-function createPooledKey(pool, tableName, key, jwt, callback) {
+async function createPooledKey(pool, tableName, key, jwt) {
+	// first, choose a key from the pool.
+	// a more sophisticated implementation would balance the load,
+	// but for demo purposes, random suffices.
+	var randomKeyFromPool =
+		pool.apiKeys[Math.floor(Math.random() * pool.apiKeys.length)]; // note a production system can do much better than random.
+	// first make sure we have a valid key ID
 
+	console.log('createPooledKey', randomKeyFromPool);
 
-  // first, choose a key from the pool.
-  // a more sophisticated implementation would balance the load,
-  // but for demo purposes, random suffices.
-  var randomKeyFromPool = pool.apiKeys[Math.floor(Math.random() * pool.apiKeys.length)]; // note a production system can do much better than random.
-  // first make sure we have a valid key ID
+	const data = await dynamo.send(
+		new GetItemCommand({
+			TableName: tableName,
+			Key: {
+				id: {
+					S: randomKeyFromPool,
+				},
+			},
+		})
+	);
 
-  
-  console.log("createPooledKey", randomKeyFromPool);
+	console.log('FoundPooledKey: ', JSON.stringify(data, 0, 2));
 
-  dynamo
-    .getItem({
-      TableName: tableName,
-      Key: {
-        id: {
-          S: randomKeyFromPool,
-        },
-      },
-    })
-    .promise()
-    .then((data) => {
-      console.log("FoundPooledKey: ", JSON.stringify(data,0,2));
-      
-      const newId = makeid(8);
-      return dynamo
-        .putItem({
-          TableName: tableName,
-          Item: {
-            id: { S: newId },
-            planId: { S: key.planId },
-            name: { S: key.name },
-            description: { S: key.description },
-            enabled: { BOOL: key.enabled },
-            owner: { S: jwt.sub },
-            value: data.Item.value,
-          },
-        })
-        .promise();
-    })
-    .then((data) => {
-      console.log("Dynamo data: ", JSON.stringify(data.Item, null, 2));
-      const response = goodResponse(
-        JSON.stringify(AWS.DynamoDB.Converter.unmarshall(data.Item))
-      );
-      callback(null, response);
-    })
-    .catch((reason) => {
-      console.error(reason);
-      callback(null, internalServerErrorResponse());
-    });
+	const newId = makeid(8);
+	await dynamo.send(
+		new PutItemCommand({
+			TableName: tableName,
+			Item: {
+				id: { S: newId },
+				planId: { S: key.planId },
+				name: { S: key.name },
+				description: { S: key.description },
+				enabled: { BOOL: key.enabled },
+				owner: { S: jwt.sub },
+				value: data.Item.value,
+			},
+		})
+	);
+
+	console.log('Dynamo data: ', JSON.stringify(data.Item, null, 2));
+  console.log( goodResponse(
+		JSON.stringify(unmarshall(data.Item))
+	))
+	return goodResponse(
+		JSON.stringify(unmarshall(data.Item))
+	);
 }
--- lambda/get_keys.js
+++ lambda/get_keys.js
@@ -1,52 +1,72 @@
-const AWS = require('aws-sdk');
-const { parseJwt, goodResponse, internalServerErrorResponse } = require('./utils');
+const { DynamoDBClient, ScanCommand } = require('@aws-sdk/client-dynamodb');
+const {
+	parseJwt,
+	goodResponse,
+	internalServerErrorResponse,
+} = require('./utils');
 
-const dynamo = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+const dynamoClient = new DynamoDBClient({ region: 'eu-north-1' }); // replace 'REGION' with your AWS region
 
-exports.handler = function(event, context, callback) {
-  console.log('Received event:', JSON.stringify(event, null, 2));
-  console.log('Received context: ', JSON.stringify(context, null, 2));
-  
-  const plansTable = process.env.PLANS_TABLE_NAME;
-  const keysTable = process.env.KEYS_TABLE_NAME;
-  
-  const httpMethod = event.httpMethod; // e.g. "GET"
-  const path = event.path;  // e.g. "/admin/plans/123456
-  const resource = event.resource;  // e.g. "/admin/plans/{id}
-  const token = event.headers.Authorization.replace(/^[Bb]earer\s+/,'').trim();
-  
-  if (!plansTable || !keysTable || !path || !resource || !httpMethod || !token ) { 
-    console.error(`HTTP 500: Precondition Fail: '${httpMethod}' '${path}' '${resource}' '${plansTable}' '${keysTable}' token.len=${token.len} `);
-    callback(null,internalServerErrorResponse());
-    return;
-  }
-  const jwt = parseJwt(token)
-  console.log('JWT payload: ', JSON.stringify(jwt, null, 2));
+exports.handler = async function (event) {
+	console.log('Received event:', JSON.stringify(event, null, 2));
 
-  getKeys(keysTable, jwt, callback); 
-}
+	const plansTable = process.env.PLANS_TABLE_NAME;
+	const keysTable = process.env.KEYS_TABLE_NAME;
+
+	const httpMethod = event.httpMethod; // e.g. "GET"
+	const path = event.path; // e.g. "/admin/plans/123456
+	const resource = event.resource; // e.g. "/admin/plans/{id}
+	if (!event.headers || !event.headers.Authorization) {
+		console.error('HTTP 500: Authorization header missing');
+		return internalServerErrorResponse();
+	}
+
+	const token = event.headers.Authorization.split(' ')[1];
+
+	if (
+		!plansTable ||
+		!keysTable ||
+		!path ||
+		!resource ||
+		!httpMethod ||
+		!token
+	) {
+		console.error(
+			`HTTP 500: Precondition Fail: '${httpMethod}' '${path}' '${resource}' '${plansTable}' '${keysTable}' token.len=${token.len} `
+		);
+		return internalServerErrorResponse();
+	}
+	const jwt = parseJwt(token);
+	console.log('JWT payload: ', JSON.stringify(jwt, null, 2));
+
+	try {
+		const response = await getKeys(keysTable, jwt);
+		return response;
+	} catch (err) {
+		console.error('HTTP 500: Dynamo responded: ', JSON.stringify(err, null, 2));
+		return internalServerErrorResponse();
+	}
+};
 
 /**
  * GET /admin/keys
  */
- function getKeys(tableName, jwt, callback) {
-  dynamo.scan({ 
-      TableName : tableName, 
-      FilterExpression : "#owner = :o",
-      ExpressionAttributeNames: {
-          "#owner": "owner"
-      },
-      ExpressionAttributeValues: {
-         ":o": {
-              S: jwt.sub
-          }
-      }
-  }).promise().then((data)=>{
-      console.log("Dynamo data: ",JSON.stringify(data.Items, null, 2));
-      const response = goodResponse(JSON.stringify(data.Items.map((item)=>AWS.DynamoDB.Converter.unmarshall(item))));
-      callback(null,response);
-  }).catch((err)=>{
-      console.error("HTTP 500: Dynamo responded: ", JSON.stringify(err, null, 2));
-      callback(null,internalServerErrorResponse());
-  });
-}  
+async function getKeys(tableName, jwt) {
+	const command = new ScanCommand({
+		TableName: tableName,
+		FilterExpression: '#owner = :o',
+		ExpressionAttributeNames: {
+			'#owner': 'owner',
+		},
+		ExpressionAttributeValues: {
+			':o': {
+				S: jwt.sub,
+			},
+		},
+	});
+
+	const data = await dynamoClient.send(command);
+	console.log('Dynamo data: ', JSON.stringify(data.Items, null, 2));
+	const response = goodResponse(JSON.stringify(data.Items));
+	return response;
+}
